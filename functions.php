529f08d4e5d1cebc738a72ae257ff10e87aeb549
luis.morales <luis.moralesponce@gmail.com>
2021-05-22 10:28:31 -0500

    order file flow

--- functions.php
+++ functions.php
@@ -13,9 +13,5 @@
   add_theme_support( 'html5', array( 'search-form', 'gallery', 'caption' ) );
   add_theme_support( 'post-thumbnails' );
   } );
-
   add_filter('show_admin_bar','__return_false');
-
-
-
   ?>
--- inc/libraries.php
+++ inc/libraries.php
@@ -3,25 +3,25 @@
 add_action( 'wp_enqueue_scripts', function() {
     $theme = wp_get_theme();
     $theme_ver = $theme->version;
-     wp_enqueue_style( 'estilos',
-      get_template_directory_uri().'/styles/css/style.min.css',
-      array(),
-      $theme_ver
+      wp_enqueue_style( 'estilos',
+        get_template_directory_uri().'/styles/css/style.min.css',
+        array(),
+        $theme_ver
       );
       wp_enqueue_style( 'aos',
-      get_template_directory_uri().'/styles/css/aos.css',
-      array(),
-      $theme_ver
+        get_template_directory_uri().'/styles/css/aos.css',
+        array(),
+        $theme_ver
       );
       wp_enqueue_style( 'owlcarousel2',
-      get_template_directory_uri().'/styles/css/owl.carousel.min.css',
-      array(),
-      $theme_ver
+        get_template_directory_uri().'/styles/css/owl.carousel.min.css',
+        array(),
+        $theme_ver
       );
       wp_enqueue_style( 'owlcarouseltheme',
-      get_template_directory_uri().'/styles/css/owl.theme.default.min.css',
-      array(),
-      $theme_ver
+        get_template_directory_uri().'/styles/css/owl.theme.default.min.css',
+        array(),
+        $theme_ver
       );
 
       wp_enqueue_script("jquery");
@@ -34,10 +34,10 @@ add_action( 'wp_enqueue_scripts', function() {
       );
 
       wp_enqueue_script( 'owl-carousel',
-      get_template_directory_uri().'/scripts/owl.carousel.min.js',
-      array( 'jquery' ),
-      $theme_ver,
-      true
+        get_template_directory_uri().'/scripts/owl.carousel.min.js',
+        array( 'jquery' ),
+        $theme_ver,
+        true
       );
       wp_enqueue_script( 'aos-script',
         get_template_directory_uri().'/scripts/aos.js',
