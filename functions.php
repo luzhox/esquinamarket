ea18457eb06649a2581bfea421b80641a4effcd7
luis.morales <luis.moralesponce@gmail.com>
2021-05-22 17:25:44 -0500

    order file flow

--- functions.php
+++ functions.php
@@ -8,10 +8,5 @@
   require('inc/libraries.php');
   require('lib/helpers.php');
   require('inc/etc.php');
-  add_action( 'after_setup_theme', function() {
-  add_theme_support( 'title-tag' );
-  add_theme_support( 'html5', array( 'search-form', 'gallery', 'caption' ) );
-  add_theme_support( 'post-thumbnails' );
-  } );
-  add_filter('show_admin_bar','__return_false');
+
   ?>
