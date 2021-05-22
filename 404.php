07b4780d2d425982007ddde2af0b9828e7d58b9d
luis.morales <luis.moralesponce@gmail.com>
2021-05-22 10:48:25 -0500

    order file flow

--- 404.php
+++ 404.php
@@ -1,27 +1,12 @@
-<?php
-    
-    get_header(); ?>
+<?php get_header(); ?>
   <body <?php body_class(); ?>>
-
 	<main role="main">
-		<!-- section -->
 		<section>
-
-			<!-- article -->
 			<article id="post-404">
-
 				<h1>La página que esta buscando no existe</h1>
-				<h2>
-					<a class="btn__primary" href="<?php echo home_url(); ?>"> Regresar a home </a>
-				</h2>
-
+				<a class="btn__primary" href="<?php echo home_url(); ?>"> Regresar a home </a>
 			</article>
-			<!-- /article -->
-
 		</section>
-		<!-- /section -->
 	</main>
   </body>
 </html>
-
-
